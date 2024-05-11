6006cb53e8cf1c868179ab23c665e66b31215736
Evelyn Lopes <113462824+EvelynLopesSS@users.noreply.github.com>
2024-05-10 23:10:07 -0300

    Add files via upload

---
+++ pdfs.py
@@ -0,0 +1,153 @@
+from langchain.chains import RetrievalQA
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain.callbacks.manager import CallbackManager
+from langchain_community.llms import Ollama
+from langchain_community.embeddings.ollama import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+from langchain.prompts import PromptTemplate
+from langchain.memory import ConversationBufferMemory
+import streamlit as st
+import os
+import time
+import base64
+
+
+if not os.path.exists('files'):
+    os.mkdir('files')
+
+if not os.path.exists('chroma'):
+    os.mkdir('chroma')
+
+if 'template' not in st.session_state:
+    st.session_state.template = """You are a knowledgeable chatbot, here to help with questions of the user. Your tone should be professional and informative.
+
+    Context: {context}
+    History: {history}
+
+    User: {question}
+    Chatbot:"""
+if 'prompt' not in st.session_state:
+    st.session_state.prompt = PromptTemplate(
+        input_variables=["history", "context", "question"],
+        template=st.session_state.template,
+    )
+if 'memory' not in st.session_state:
+    st.session_state.memory = ConversationBufferMemory(
+        memory_key="history",
+        return_messages=True,
+        input_key="question")
+if 'vectorstore' not in st.session_state:
+    st.session_state.vectorstore = Chroma(persist_directory='local-rag',
+                                          embedding_function=OllamaEmbeddings(model="nomic-embed-text")
+                                          )
+if 'llm' not in st.session_state:
+    st.session_state.llm = Ollama(model="mistral",
+                                  verbose=True,
+                                  callback_manager=CallbackManager([StreamingStdOutCallbackHandler()]),
+                                  )
+
+def load_image_to_base64(image_path):
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode()
+    
+user_avatar_path = 'user.png'
+assistant_avatar_path = 'assistente.png'
+
+# Carregar os avatares em base64
+user_avatar_base64 = load_image_to_base64(user_avatar_path)
+assistant_avatar_base64 = load_image_to_base64(assistant_avatar_path)
+# Initialize session state
+if 'chat_history' not in st.session_state:
+    st.session_state.chat_history = []
+
+st.title("PDF Assistant")
+
+# Upload a PDF file
+st.sidebar.image('logo.png', use_column_width=True)
+
+st.markdown("""
+    ## How to Use This Application
+    - **Drag and drop** your PDF file into the designated area or use the upload button below.
+    - Once you see a message stating your document has been processed, you can start asking questions in the chat input to interact with the PDF content.
+    ---
+
+""", unsafe_allow_html=True)
+
+uploaded_file = st.sidebar.file_uploader("Upload your PDF", type='pdf')
+
+for message in st.session_state.chat_history:
+    with st.chat_message(message["role"]):
+        st.markdown(message["message"])
+
+if uploaded_file is not None:
+    if not os.path.isfile("files/"+uploaded_file.name+".pdf"):
+        with st.status("Processing your document..."):
+            bytes_data = uploaded_file.read()
+            f = open("files/"+uploaded_file.name+".pdf", "wb")
+            f.write(bytes_data)
+            f.close()
+            loader = PyPDFLoader("files/"+uploaded_file.name+".pdf")
+            data = loader.load()
+
+            # Initialize text splitter
+            text_splitter = RecursiveCharacterTextSplitter(
+                chunk_size=1500,
+                chunk_overlap=200,
+                length_function=len
+            )
+            all_chunks = text_splitter.split_documents(data)
+
+            # Create and persist the vector store
+            st.session_state.vectorstore = Chroma.from_documents(
+                documents=all_chunks,
+                embedding=OllamaEmbeddings(model="mistral")
+            )
+            st.session_state.vectorstore.persist()
+            st.success("Your document has been processed.")
+            st.balloons()
+
+
+    st.session_state.retriever = st.session_state.vectorstore.as_retriever()
+    # Initialize the QA chain
+    if 'qa_chain' not in st.session_state:
+        st.session_state.qa_chain = RetrievalQA.from_chain_type(
+            llm=st.session_state.llm,
+            chain_type='stuff',
+            retriever=st.session_state.retriever,
+            verbose=True,
+            chain_type_kwargs={
+                "verbose": True,
+                "prompt": st.session_state.prompt,
+                "memory": st.session_state.memory,
+            }
+        )
+
+    # Chat input
+    if user_input := st.chat_input("You:", key="user_input"):
+        user_message = {"role": "user", "message": user_input}
+        st.session_state.chat_history.append(user_message)
+        with st.chat_message("user",avatar=f"data:image/png;base64,{user_avatar_base64 }"):
+            st.write("User’s question 🗣️")
+            st.markdown(user_input)
+        with st.chat_message("assistant", avatar=f"data:image/png;base64,{assistant_avatar_base64}"):
+            st.write("Assistant's response 🤖")
+            with st.spinner("Assistant is typing..."):
+                response = st.session_state.qa_chain(user_input)
+            message_placeholder = st.empty()
+            full_response = ""
+            for chunk in response['result'].split():
+                full_response += chunk + " "
+                time.sleep(0.05)
+                # Add a blinking cursor to simulate typing
+                message_placeholder.markdown(full_response + "▌")
+            message_placeholder.markdown(full_response)
+
+        chatbot_message = {"role": "assistant", "message": response['result']}
+        st.session_state.chat_history.append(chatbot_message)
+
+
+else:
+
+    st.warning("Please upload a PDF file.", icon="⚠️")
